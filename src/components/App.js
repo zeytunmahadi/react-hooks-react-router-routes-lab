f2f7e712d7695ac6479c98ab0d231caf75105781
zeitun <zeytunmahadi718@gmail.com>
2024-07-21 09:08:56 +0300

    creating route for my app

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,25 @@ import Directors from "./Directors";
 import Movies from "./Movies";
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route exact path="/movies">
+          <Movies />
+        </Route>
+        <Route exact path="/actors">
+          <Actors />
+        </Route>
+        <Route exact path="/directors">
+          <Directors />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,24 @@
+
 import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar">
+      <NavLink to="/" exact>
+        Home
+      </NavLink>
+      <NavLink to="/directors" exact>
+        Directors
+      </NavLink>
+      <NavLink to="/actors" exact>
+        Actors
+      </NavLink>
+      <NavLink to="/movies" exact>
+        Movies
+      </NavLink>
+    </div>
+  );
 }
 
 export default NavBar;
